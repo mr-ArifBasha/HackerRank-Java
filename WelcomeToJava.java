310d70cbbb061fdf6b673d6a81046202921c59f2
Arif Basha <103433591+mr-ArifBasha@users.noreply.github.com>
2022-08-01 18:42:30 +0530

    Create WelcomeToJava.java

---
+++ WelcomeToJava.java
@@ -0,0 +1,11 @@
+//Arif Basha 01/08/2022.
+
+public class WelcomeToJava {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+        
+    }
+}
